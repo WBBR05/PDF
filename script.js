a097c05244f8863a6ec78d73f4e075d79716dce3
Peťko Franta <p.franta05@gmail.com>
2024-02-13 19:59:20 +0100

    PDF funkuguje

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Datum
 const currentDate = new Date();
 const formattedDate = `K datumu ${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`;
 document.getElementById("datum").innerHTML = formattedDate;
+//PDF stiahnutie
+var opt = {
+    filename: 'Výdajka.pdf',
+    margin: 0.05,
+    html2canvas: { scale: 2},
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+}
+function generatePDF(){
+    const element = document.getElementById("pdf");
+    html2pdf().set(opt).from(element).save();
+}
